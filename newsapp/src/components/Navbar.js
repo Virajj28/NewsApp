6f6dcff27d67caaebc7122d6c58c1c444f8a98cf
Viraj Jadhav <viraj28j@gmail.com>
2021-09-17 13:03:25 +0530

    navbar descrip added

---
+++ newsapp/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="container border bg-gray-400">
+            This is A News App - 
+            Explore the News Daily for free about sports,politics,entertainment,technology,science,health,education,and more.
+        </div>
+    )
+}
+
+export default Navbar
